aa62ae26a1d43a331291bf99c826a9572147f7ff
James Maye <jamesmaye6094@gmail.com>
2023-06-10 00:24:54 -0400

    add entity model

---
+++ returnGame/src/model/Entity.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Entity {
+	private String name;
+	private float healthPoints;
+	private float manaPoints;
+	
+	public Entity(String name) {
+		this.name = name;
+	}
+	
+	public Entity(String name, float healthPoints) {
+		this.name = name;
+		this.healthPoints = healthPoints;
+	}
+	
+	public Entity(String name, float healthPoints, float manaPoints) {
+		this.name = name;
+		this.healthPoints = healthPoints;
+		this.manaPoints = manaPoints;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getHealthPoints() {
+		return healthPoints;
+	}
+
+	public void setHealthPoints(float healthPoints) {
+		this.healthPoints = healthPoints;
+	}
+
+	public float getManaPoints() {
+		return manaPoints;
+	}
+
+	public void setManaPoints(float manaPoints) {
+		this.manaPoints = manaPoints;
+	}
+	
+	
+
+}
